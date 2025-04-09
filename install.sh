9f3b69e9124eea65946d19fad7991c30363f40a1
piotr310100 <56043614+piotr310100@users.noreply.github.com>
2025-04-09 19:41:42 +0200

    Add option to install cuda with conda

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-conda create -n dmiso python=3.8 pip
-conda activate dmiso
+CONDA_CUDA=false
+
+for arg in "$@"; do
+  if [ "$arg" == "--conda-cuda" ]; then
+    CONDA_CUDA=true
+  fi
+done
+
+conda create -n dmiso python=3.8 pip -y
+
+if ["$CONDA_CUDA" = true]; then
+  conda install -n dmiso cuda -c nvidia/label/cuda-11.8.0 -y
+
 conda run -n dmiso pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 conda run -n dmiso pip install submodules/diff-gaussian-rasterization
 conda env update -n dmiso --file environment.yml
