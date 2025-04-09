fae3cd391ae186c0ed3920eea6996c8d92279772
piotr310100 <56043614+piotr310100@users.noreply.github.com>
2025-04-09 19:44:08 +0200

    Fix if

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ conda create -n dmiso python=3.8 pip -y
 
 if ["$CONDA_CUDA" = true]; then
   conda install -n dmiso cuda -c nvidia/label/cuda-11.8.0 -y
+fi
 
 conda run -n dmiso pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 conda run -n dmiso pip install submodules/diff-gaussian-rasterization
